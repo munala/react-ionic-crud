02979cb8c323d46f45644b54fe9ad115df3f2e44
munala <munalaoliverngoitsi@gmail.com>
2020-07-12 19:26:18 +0300

    add pagination to colors screen

--- src/pages/Colors/index.tsx
+++ src/pages/Colors/index.tsx
@@ -7,7 +7,10 @@ import {
   IonSelectOption,
   IonSelect,
   IonButton,
+  IonListHeader,
 } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+
 import AppContext from '../../context/state';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import { ColorInterface } from '../../typescript/interfaces';
@@ -20,6 +23,7 @@ const MAX_PER_PAGE = 20;
 const Colors: React.FC = () => {
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(MAX_PER_PAGE);
+  const history = useHistory();
 
   // generate and fill array with values from 1 to MAX_PER_PAGE
   let numberArray = new Array(MAX_PER_PAGE);
@@ -49,6 +53,10 @@ const Colors: React.FC = () => {
     setPerPage(parseInt(value, 10));
   };
 
+  const onItemClick = (id: string | undefined) => {
+    history.push(`/colors/${id}`);
+  };
+
   if (loading) return <LoadingIndicator />;
 
   return (
@@ -59,44 +67,50 @@ const Colors: React.FC = () => {
         </IonItem>
       )}
       <IonList>
-        <IonItem>
+        <IonListHeader>
           <IonLabel>NAME</IonLabel>
           <IonLabel>YEAR</IonLabel>
           <IonLabel>PANTONE VALUE</IonLabel>
-        </IonItem>
+        </IonListHeader>
 
         {colors.map((color: ColorInterface) => (
-          <IonItem key={color.id} style={{ backgroundColor: color.color }}>
+          <IonItem
+            key={color.id}
+            button
+            onClick={() => onItemClick(color.id)}
+            style={{ backgroundColor: color.color }}
+          >
             <IonLabel>{color.name}</IonLabel>
             <IonLabel>{color.year}</IonLabel>
             <IonLabel>{color.pantone_value || ''}</IonLabel>
           </IonItem>
         ))}
-
-        <IonItem>
-          <IonLabel>Colors per page</IonLabel>
-          <IonSelect
-            value={perPage}
-            placeholder="Select number of colors per page"
-            onIonChange={(e) => selectPerPageValue(e.detail.value)}
-          >
-            {numberArray.map((perPageValue: number) => (
-              <IonSelectOption key={perPageValue} value={perPageValue}>
-                {perPageValue}
-              </IonSelectOption>
-            ))}
-          </IonSelect>
-        </IonItem>
-        <IonItem>
-          <IonButton disabled={page === 1} onClick={() => setPage(page - 1)} color="light">
-            Previous
-          </IonButton>
-          <IonLabel>{` Page ${page} `}</IonLabel>
-          <IonButton disabled={colors.length === 0} onClick={() => setPage(page + 1)} color="light">
-            Next
-          </IonButton>
-        </IonItem>
       </IonList>
+
+      <IonItem>
+        <IonLabel>Colors per page</IonLabel>
+        <IonSelect
+          value={perPage}
+          placeholder="Select number of colors per page"
+          onIonChange={(e) => selectPerPageValue(e.detail.value)}
+        >
+          {numberArray.map((perPageValue: number) => (
+            <IonSelectOption key={perPageValue} value={perPageValue}>
+              {perPageValue}
+            </IonSelectOption>
+          ))}
+        </IonSelect>
+      </IonItem>
+
+      <IonItem>
+        <IonButton disabled={page === 1} onClick={() => setPage(page - 1)} color="light">
+          Previous
+        </IonButton>
+        <IonLabel>{` Page ${page} `}</IonLabel>
+        <IonButton disabled={colors.length === 0} onClick={() => setPage(page + 1)} color="light">
+          Next
+        </IonButton>
+      </IonItem>
     </IonContent>
   );
 };
